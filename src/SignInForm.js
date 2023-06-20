8f8fe6f3c3c7198a1e54048bee8d738e998b7120
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-06-20 10:41:33 +0530

    Formatted with prettier

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -4,22 +4,33 @@ export default function SignInForm() {
     <div className="login form">
       <HeadComponent></HeadComponent>
       <body>
-      <InputComponent
+      <tr>
+      <td><InputComponent
           lable={"First Name"}
           placeholder={"Enter first name"}
         ></InputComponent>
+        </td>
+        <td>
         <InputComponent
           lable={"Last Name"}
           placeholder={"Enter last name"}
         ></InputComponent>
+        </td>
+      </tr>
+      <tr>
+        <td>
          <InputComponent
           lable={"Email Id"}
           placeholder={"e.g john.smith@org.name"}
         ></InputComponent>
+        </td>
+        <td>
         <InputComponent
           lable={"Phone Number"}
           placeholder={"+91"}
         ></InputComponent>
+        </td>
+        </tr>
         <ButtonComponent type={"submit"} lable={"Cancel"}></ButtonComponent>
         <ButtonComponent type={"reset"} lable={"Submit"}></ButtonComponent>
       </body>
